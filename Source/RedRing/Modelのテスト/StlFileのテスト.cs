e6f39f6c28bb59be07f2d6b5bc4ce34da670f14a
nitta mitsuhiro <potimarimo@gmail.com>
2014-09-15 20:16:54 +0900

    自動テスト用テストデータの位置を変更

--- Source/RedRing/Modelのテスト/StlFileのテスト.cs
+++ Source/RedRing/Modelのテスト/StlFileのテスト.cs
@@ -14,15 +14,15 @@ namespace Marimo.RedRing.Model.Test
         [Test]
         public async Task LoadAsciiAsyncは適切なファイルの読み込み時にポリゴンを返す()
         {
-            Expect(await StlFile.LoadAsciiAsync(@"..\..\..\..\..\SampleData\StlAscii\cube-ascii.stl"), Is.Not.Null);
+            Expect(await StlFile.LoadAsciiAsync(@"..\..\TestData\StlAscii\cube-ascii.stl"), Is.Not.Null);
 
-            Expect(await StlFile.LoadAsciiAsync(@"..\..\..\..\..\SampleData\StlAscii\singleFacet-ascii.stl"), Is.Not.Null);
+            Expect(await StlFile.LoadAsciiAsync(@"..\..\TestData\StlAscii\singleFacet-ascii.stl"), Is.Not.Null);
         }
 
         [Test]
         public async Task LoadBinaryAsyncは適切なファイルの読み込み時にポリゴンを返す()
         {
-            Expect(await StlFile.LoadBinaryAsync(@"..\..\..\..\..\SampleData\StlBinary\cube-binary.stl"), Is.Not.Null);
+            Expect(await StlFile.LoadBinaryAsync(@"..\..\TestData\StlBinary\cube-binary.stl"), Is.Not.Null);
         }
     }
 }
