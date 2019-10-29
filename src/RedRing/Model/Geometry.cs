01d49dc2dd4e161c71c6a19b4eac2ff36bda5cd6
高津則幸 <takatsu.noriyuki@gmail.com>
2019-10-29 23:49:14 +0900

    不要な変数を削除

--- src/RedRing/Model/Geometry.cs
+++ src/RedRing/Model/Geometry.cs
@@ -11,11 +11,6 @@ namespace RedRing.Model
 
         public IEnumerable<ベクトル> Normals { get;private set; }
 
-        /// <summary>
-        /// 三角形の頂点法線ベクトル
-        /// </summary>
-        public IEnumerable<ベクトル> VertexNormals { get; private set; }
-
         public Geometry(IEnumerable<ベクトル> positions, IEnumerable<Tuple<int, int, int>> triangleIndices)
             :this(positions, triangleIndices, new ベクトル[] { })
         {
