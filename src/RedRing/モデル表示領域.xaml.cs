c81525787cfa26e3d692bb1af65a451238a1e965
高津則幸 <takatsu.noriyuki@gmail.com>
2020-03-15 16:22:28 +0900

    ファイル構成の変更 モデル回転機能の実装

---
+++ src/RedRing/モデル表示領域.xaml.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Media3D;
+using System.Windows.Threading;
+using RedRing.Model;
+
+namespace RedRing
+{
+    /// <summary>
+    /// モデル表示領域.xaml の相互作用ロジック
+    /// </summary>
+    public partial class モデル表示領域 : UserControl
+    {
+        DispatcherTimer timer = new DispatcherTimer { Interval = new TimeSpan(200) };
+        private Matrix3D matrix = Matrix3D.Identity;
+        private bool isDrag = false;
+        private Point offset;
+        const double AngleRatio = 0.5;
+
+        public new 空間 DataContext
+        {
+            get
+            {
+                return base.DataContext as 空間;
+            }
+            set
+            {
+                base.DataContext = value;
+            }
+        }
+
+        public モデル表示領域()
+        {
+            InitializeComponent();
+
+            timer.Tick +=
+                (sender, e) =>
+            {
+                group.Children.Clear();
+
+                group.Children.Add(
+                    new DirectionalLight
+                    {
+                        Color = Colors.White,
+                        Direction = new Vector3D(0, -0.5, -0.6)
+                    });
+
+                if (DataContext == null)
+                {
+                    return;
+                }
+
+                foreach (var 表示要素 in DataContext.表示モデル)
+                {
+                    group.Children.Add(
+                    new GeometryModel3D
+                    {
+                        Geometry =
+                    new MeshGeometry3D
+                    {
+                        Positions = new Point3DCollection(
+                                from position in ((表示要素.Geometry as Model.Geometry)).Positions
+                                select new Point3D(position.X, position.Y, position.Z)),
+                        TriangleIndices = new Int32Collection(
+                                from triangleIndex in ((表示要素.Geometry as Model.Geometry)).TriangleIndices
+                                from index in new[] { triangleIndex.Item1, triangleIndex.Item2, triangleIndex.Item3 }
+                                select index),
+                        Normals = new Vector3DCollection()
+                    },
+                        Material = new DiffuseMaterial { Brush = new SolidColorBrush(Colors.Pink) }
+                    }
+                    );
+                }
+            };
+
+            //var events = new EventsExtension<Grid>(model);
+            //events.MouseDragDelta.Subscribe(ViewModel.RotateDelta);
+        }
+
+        private void Viewport3D_Loaded(object sender, RoutedEventArgs e)
+        {
+            timer.Start();
+        }
+
+        private void mouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            isDrag = true;
+            offset = e.GetPosition(this);
+        }
+
+        // 原点から delta までの符号付き距離を求めます。
+        static double GetDistance(Vector start, Vector delta)
+        {
+            var angle = Vector.AngleBetween(delta, start);
+            return start.Length * Math.Sin(angle * Math.PI / 180);
+        }
+
+        private void mouseMove(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            if (isDrag == true)
+            {
+                var delta = e.GetPosition(this) - offset;
+                var deltaLength = delta.Length;
+                if (deltaLength == 0) return;
+
+                var distance = GetDistance((Vector)offset, delta);
+                matrixTransform.Rotate(new Vector3D(delta.Y, delta.X, distance), AngleRatio * deltaLength);
+            }
+
+        }
+
+        private void mouseLeftButtonUp(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            isDrag = false;
+        }
+
+        private void mouseLeave(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            isDrag = false;
+        }
+    }
+
+    public static class Media3DUtility
+    {
+        public static void Rotate(this MatrixTransform3D transform, Vector3D axis, double angle)
+        {
+            var matrix = transform.Matrix;
+            matrix.Rotate(new Quaternion(axis, angle));
+            transform.Matrix = matrix;
+        }
+    }
+}
