d8e2cb5b7df5cf8f7be7e723319d8f47ca93ef2b
高津則幸 <takatsu.noriyuki@gmail.com>
2021-09-23 23:17:29 +0900

    不要なusing宣言を削除

--- src/Framework/IO/Output/STLFile.cs
+++ src/Framework/IO/Output/STLFile.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using RedRing.Framework.Geometry.Double.Geometry3D;
