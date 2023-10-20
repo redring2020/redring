c8ad147b39f4fa086a6624602bd6cc4e27393bfe
RedRing <info@redring.jp>
2023-10-20 12:29:24 +0900

    ニュートン・ラプソン法のリファクタリング

---
+++ src/Common/Numerics/RootFinding/NewtonRaphson.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using static RedRing.Common.Numerics.CalculationDefine;
+
+namespace RedRing.Common.Numerics
+{
+    public class NewtonRaphson
+    {
+        const int Limit = 1000;
+        static double DifferentialEquation(Func<double, double> func, double xd)
+        {
+            double h = Pow(10.0, -9.0);
+            return (func(xd + h) - func(xd)) / h;
+        }
+
+        public static double FindRoot(Func<double, double> func, double x, double eps = EPS, int limit = Limit)
+        {
+            if (TryFindRoot(func, x, eps, limit, out var root))
+            {
+                return root;
+            }
+            throw new Exception();
+        }
+
+        static bool TryFindRoot(Func<double, double> func, double x, double eps, int limit, out double root)
+        {
+            if (eps <= 0)
+            {
+                throw new ArgumentOutOfRangeException("eps");
+            }
+
+            try
+            {
+                root = x;
+                for (int i = 0; i < limit; i++)
+                {
+                    root = x - func(x) / DifferentialEquation(func, x);
+                    if (Abs(root - x) < eps)
+                    {
+                        return true;
+                    }
+                    x = root;
+                }
+            }
+            catch
+            {
+                throw new Exception();
+            }
+
+            return false;
+        }
+    }
+}
--- src/UnitTest/Common/TestNumerics/UnitTestNewtonRaphshonMethod.cs
+++ src/UnitTest/Common/TestNumerics/UnitTestNewtonRaphshonMethod.cs
@@ -11,11 +11,11 @@ namespace RedRing.UnitTest.Common.TestNumerics
         {
             Func<double, double> func = (double x) => (Pow(x, 2.0) - 1.0);
 
-            var ans1 = NewtonRaphsonMethod.Calc(func, 2.0);
+            var ans1 = NewtonRaphson.FindRoot(func, 2.0);
             Assert.IsNotNull(ans1);
             Assert.AreEqual(ans1, 1.0);
 
-            var ans2 = NewtonRaphsonMethod.Calc(func, -2.0);
+            var ans2 = NewtonRaphson.FindRoot(func, -2.0);
             Assert.IsNotNull(ans2);
             Assert.AreEqual(ans2, -1.0);
         }
