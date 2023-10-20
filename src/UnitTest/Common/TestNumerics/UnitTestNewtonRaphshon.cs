e3761540664e3f06cc97fdc220f04291d128ea2a
RedRing <info@redring.jp>
2023-10-20 12:52:14 +0900

    テスト修正

--- src/UnitTest/Common/TestNumerics/UnitTestNewtonRaphshon.cs
+++ src/UnitTest/Common/TestNumerics/UnitTestNewtonRaphshon.cs
@@ -4,7 +4,7 @@ using RedRing.Common.Numerics;
 namespace RedRing.UnitTest.Common.TestNumerics
 {
     [TestClass]
-    public class UnitTestNewtonRaphshonMethod
+    public class UnitTestNewtonRaphshon
     {
         [TestMethod]
         public void TestNewtonRaphshonMethod()
@@ -12,11 +12,9 @@ namespace RedRing.UnitTest.Common.TestNumerics
             Func<double, double> func = (double x) => (Pow(x, 2.0) - 1.0);
 
             var ans1 = NewtonRaphson.FindRoot(func, 2.0);
-            Assert.IsNotNull(ans1);
             Assert.AreEqual(ans1, 1.0);
 
             var ans2 = NewtonRaphson.FindRoot(func, -2.0);
-            Assert.IsNotNull(ans2);
             Assert.AreEqual(ans2, -1.0);
         }
     }
